996ddf534f738de33683d183e8f5ba22c8a69132
CalimeRon <ron.chamma@gmail.com>
2021-06-09 19:53:01 +0300

    Add simple MapView

--- client/containers/App.js
+++ client/containers/App.js
@@ -1,12 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import MapComponent from '../components/MapComponent'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <MapComponent />
       <StatusBar style="auto" />
+      <Text>Hello</Text>
     </View>
   );
 }
