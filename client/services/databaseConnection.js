7306703887a0f67dba210c0a40524f356b065be4
Calimeron <ron.chamma@gmail.com>
2021-07-10 15:57:48 +0300

    feat: env variables
    Configured environment variables and
    an example .env to help setting up the app

--- client/services/databaseConnection.js
+++ client/services/databaseConnection.js
@@ -1,20 +1,21 @@
-import firebase from 'firebase/app';
-import 'firebase/firestore';
+import firebase from "firebase/app";
+import "firebase/firestore";
+import { API_KEY, APP_ID } from "@env";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBwFk0rmjv0JHcUzWtXV_ZpnoYQDR7MVTU",
+  apiKey: API_KEY,
   authDomain: "handicrawl.firebaseapp.com",
-  databaseURL: "https://handicrawl-default-rtdb.europe-west1.firebasedatabase.app",
+  databaseURL:
+    "https://handicrawl-default-rtdb.europe-west1.firebasedatabase.app",
   projectId: "handicrawl",
   storageBucket: "handicrawl.appspot.com",
   messagingSenderId: "600608599441",
-  appId: "1:600608599441:web:c2fd37eb5aa4ae4c66fe30",
-  measurementId: "G-1DXE8MV8EG"
+  appId: APP_ID,
+  measurementId: "G-1DXE8MV8EG",
 };
 
 firebase.initializeApp(firebaseConfig);
 
 const dbh = firebase.firestore();
-
 
 export default dbh;
